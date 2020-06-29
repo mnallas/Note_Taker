c47f0847db86bb471610de24cfe759ca9f437653
myhkas <mmyhkas@gmail.com>
2020-06-28 22:04:46 -0700

    clientroutes nad get req are working

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,6 +1,24 @@
 const express = require("express");
-const route = express.Router();
+const router = express.Router();
 const fs = require("fs");
-const path = require("path");
+const util = require("util");
+
+const readFileSync = util.promisify(fs.readFile);
+const writeFileSync = util.promisify(fs.writeFile);
+
+router.get("/api/notes", async (req, res) => {
+  let countData = await readFileSync("./db/db.json");
+  countData = JSON.parse(countData);
+  res.send(countData);
+});
+
+router.post("/api/notes", async (req, res) => {
+  let notesData = await readFileSync("./db/db.json");
+  notesData = JSON.parse(notesData);
+  const { name, type, moves } = req.body;
+  pokemonData.pokemon.push({ name, id, type, moves });
+  await writeFileSync("data.json", JSON.stringify(pokemonData, null, 2));
+  res.json({ msg: "Pokemon added!" });
+});
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("./public/assets/"));
+app.use(express.static("./public/"));
 
-app.listen(
-  (PORT = () => {
-    console.log(`listening to http://localhost:${PORT}`);
-  })
-);
+const apiRoutes = require("./routes/api-routes");
+app.use(apiRoutes);
+
+const clientRoutes = require("./routes/client-routes");
+app.use(clientRoutes);
+
+app.listen(PORT, () => console.log(`listening at: http://localhost:${PORT}`));
